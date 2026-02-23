90f8586e8e9545113649538c8f1d361fc0ec3056
Alef <aleftorresbr@gmail.com>
2026-02-23 15:23:46 -0300

    new: criando novos mecanismos de login e de criação de pedidos

--- auth_routes.py
+++ auth_routes.py
@@ -7,11 +7,23 @@ from database import get_db
 
 auth_router = APIRouter(prefix="/auth", tags=["auth"])
 
+
 # Definindo um modelo Pydantic para receber os dados com segurança no corpo da requisição
 class UsuarioCreate(BaseModel):
     email: str
     senha: str
     nome: str
+
+
+class UsuarioLogin(BaseModel):
+    email: str
+    senha: str
+
+
+def criar_token(id):
+    token = f"jfiejfiajefjf{id}"
+    return token
+
 
 @auth_router.get("/", tags=["auth"])
 async def autenticar():
@@ -19,23 +31,38 @@ async def autenticar():
 
 
 @auth_router.post("/criar-usuario", status_code=status.HTTP_201_CREATED, tags=["auth"])
-async def criar_usuario(usuario: UsuarioCreate, db: Session = Depends(get_db)):
-    # Agora acessamos via usuario.email, usuario.senha, etc.
-    usuario_existente = db.query(User).filter(User.email == usuario.email).first()
+async def criar_usuario(usuario_create: UsuarioCreate, db: Session = Depends(get_db)):
+    usuario_existente = db.query(User).filter(User.email == usuario_create.email).first()
 
     if usuario_existente:
         raise HTTPException(
+
             status_code=status.HTTP_400_BAD_REQUEST,
             detail="Erro ao cadastrar usuário: Email já cadastrado"
         )
-    
+
     # Gerando o hash da senha
-    senhaCripto = bcrypt_context.hash(usuario.senha)
-    
-    novo_usuario = User(nome=usuario.nome, email=usuario.email, senha=senhaCripto)
+    senhaCripto = bcrypt_context.hash(usuario_create.senha)
+
+    novo_usuario = User(nome=usuario_create.nome, email=usuario_create.email, senha=senhaCripto)
     db.add(novo_usuario)
     db.commit()
     db.refresh(novo_usuario)
 
     return {"message": "Usuário criado com sucesso",
             "usuario": {"nome": novo_usuario.nome, "email": novo_usuario.email}}
+
+
+@auth_router.post("/login", status_code=status.HTTP_200_OK, tags=["auth"])
+async def login(usuario_login: UsuarioLogin, db: Session = Depends(get_db)):
+    usuario_existente = db.query(User).filter(User.email == usuario_login.email).first()
+
+    if not usuario_existente:
+        raise HTTPException(
+
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Erro ao fazer login, usuário não existente"
+        )
+    else:
+        access_token = criar_token(usuario_login.id)
+        return {"access_token": access_token, "token_type": "bearer"}
--- order_routes.py
+++ order_routes.py
@@ -1,7 +1,26 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+from sqlalchemy.orm import Session
+from database import get_db
+from pydantic import BaseModel
+
+from models import Order
 
 order_router = APIRouter(prefix="/order", tags=["order"])
+
 
 @order_router.get("/", tags=["order"])
 async def pedidos():
     return {"message": "Hello World"}
+
+
+class PedidoCreate(BaseModel):
+    id_usuario: int
+
+
+@order_router.post("/pedido", tags=["order"])
+async def criar_pedido(pedido: PedidoCreate, db: Session = Depends(get_db)):
+    novo_pedido = Order(pedido.id_usuario)
+    db.add(novo_pedido)
+    db.commit()
+    db.refresh(novo_pedido)
+    return {"message": "Pedido criado com sucesso", "pedido_id": novo_pedido.id}
